16c8fa774c085c38531bb7966c286ceb3bd56744
Angelo Kim Hui V. Lim <82715920+angelokimhui@users.noreply.github.com>
2024-01-05 16:27:48 +0800

    Update custom_sync.sh
    Fixup!

--- custom_sync.sh
+++ custom_sync.sh
@@ -7,4 +7,4 @@ rm -rf android/device/xiaomi/sm8250-common
 git clone https://github.com/apollo-pe/kernel_xiaomi_sm8250.git android/kernel/xiaomi/apollo
 git clone https://github.com/apollo-pe/device_xiaomi_apollo.git android/device/xiaomi/apollo
 git clone https://github.com/apollo-pe/device_xiaomi_sm8250-common.git android/device/xiaomi/sm8250-common
-git clone https://github.com/apollo-pe/android_packages_apps_GCamGOPrebuilt.git
+git clone https://github.com/apollo-pe/android_packages_apps_GCamGOPrebuilt.git android/packages/apps/GCamGOPrebuilt
